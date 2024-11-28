53a06b23bc281d6d15ff85e5c2b4a86a7a2b40ac
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-11-28 19:08:16 +0400

    First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,25 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = {20, 34, 36, 60, 68};
+        // {68,60,36,34,20}
+        System.out.println(Arrays.toString(numbers));
+        task(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+    public static void task(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length / 2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
 
     }
 }
